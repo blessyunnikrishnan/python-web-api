0b65ac64b2d00ab03d2c4665a7461c73e399bebb
Blessy Unnikrishnan <blessyunnikrishnan@gmail.com>
2024-11-26 11:39:35 +0000

    Added new feature

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    return "Hello, I am Blessy Unnikrishnan. Welcome to our team AURA!"
+    return " Welcome to our team AURA!"
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0')
