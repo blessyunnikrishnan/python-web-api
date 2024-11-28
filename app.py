ca1c1682e41b6e112627d96c68f75b2596be1a65
Blessy Unnikrishnan <blessyunnikrishnan@gmail.com>
2024-11-28 23:13:38 +0000

    Added new feature

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    return "Welcome to our team AURA!"
+    return "Hello Everyone, Welcome to our team AURA!"
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0')
